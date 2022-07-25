5db3c2f8354c96f02b03e9a52e66ba97e22f79c1
LeeJin Sim <s2011@e-mirim.hs.kr>
2022-07-25 10:53:40 +0900

    ⚡ :: 메인페이지 접속 시 html 파일 전송

--- demo.py
+++ demo.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 app = FastAPI()
 
 @app.get("/")
 def 작명():
-    return 'hello'
+    return FileResponse('index.html')
 
+'''
 @app.get("/data")
 def 작명():
     return {'hello' : 1234}
+
+    /docs 접속 시 api 문서를 자동으로 만들어준다.
+'''
+
